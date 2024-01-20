fe2f638fefbdf82451cd13db544804ee5ba5a309
libra625 <18.12.17.moyo@gmail.com>
2024-01-20 20:12:42 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const add = function (number1, number2) {
+  return number1 + number2;
+};
+
+const minus = function (number1, number2) {
+  return number1 - number2;
+};
+
+const multiply = function (number1, number2) {
+  return number1 * number2;
+};
+
+console.log(add(5, 8));
+console.log(minus(5, 8));
+console.log(multiply(5, 8));
